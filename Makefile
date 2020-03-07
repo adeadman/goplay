4a22ffc92908d7eef2793930c82b1eb351820166
Aaron Deadman <aaron@introspection.net.au>
2020-03-07 22:50:11 +0000

    Add main and Makefile

---
+++ Makefile
@@ -0,0 +1,15 @@
+build:
+	go build -o bin/goplay main.go
+
+setup: 
+	go get ./...
+	go get github.com/stretchr/testify/assert
+
+fmt:
+	gofmt -s -w .
+
+test:
+	go test -v -race -coverpkg=./... -coverprofile=coverage.txt -covermode=atomic ./...
+
+run:	build
+	bin/goplay
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, %s", r.URL.Path[1:])
+}
+
+func main() {
+	fmt.Println("Listening on port 8080")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
