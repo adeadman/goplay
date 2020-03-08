fad40a307c2a7d0ac40b3cfc0b6fc446aa618998
Aaron Deadman <aaron@introspection.net.au>
2020-03-08 19:52:51 +0000

    Refactor input/output methods and add nice buttons

--- main.go
+++ main.go
@@ -19,6 +19,33 @@ var mpg123Stdout io.ReadCloser
 var mpg123Stderr io.ReadCloser
 var mpg123StdoutChan chan string
 
+func getLinesFromStdoutMatchingPrefix(prefix string) (lines []string) {
+	for {
+		select {
+		case line := <-mpg123StdoutChan:
+			if strings.HasPrefix(line, prefix) {
+				lines = append(lines, line)
+			}
+			time.Sleep(1 * time.Millisecond)
+			continue
+		default:
+		}
+		break
+	}
+	return
+}
+
+func drainStdout() {
+	// drain the stdout channel to remove any unread stdout lines
+	_ = getLinesFromStdoutMatchingPrefix("")
+}
+
+func sendCommand(command string) {
+	io.WriteString(mpg123Stdin, fmt.Sprintf("%s\n", command))
+	// allow time for the application to read the input and respond
+	time.Sleep(10 * time.Millisecond)
+}
+
 func player(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path[1:] != "" {
 		http.Error(w, "404 not found", http.StatusNotFound)
@@ -33,30 +60,36 @@ func player(w http.ResponseWriter, r *http.Request) {
 }
 
 func play(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(mpg123Stdin, "LOAD The Shadows - Apache 1963.mp3\n")
+	sendCommand("LOAD The Shadows - Apache 1963.mp3")
 	fmt.Fprintf(w, "OK")
 }
 
 func pause(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(mpg123Stdin, "PAUSE\n")
-	fmt.Fprintf(w, "OK")
+	drainStdout()
+	sendCommand("PAUSE")
+	pauseLines := getLinesFromStdoutMatchingPrefix("@P ")
+	if len(pauseLines) > 0 {
+		if pauseLines[0] == "@P 0" {
+			fmt.Fprintf(w, "NOT PLAYING")
+		} else if pauseLines[0] == "@P 2" {
+			fmt.Fprintf(w, "RESUMED")
+		} else {
+			fmt.Fprintf(w, "?????")
+		}
+	} else {
+		fmt.Fprintf(w, "PAUSED")
+	}
 }
 
 func getPlaybackInfo(w http.ResponseWriter, r *http.Request) {
 	// get the most recent frame info
-	var recentStatusLine string
-	for {
-		select {
-		case line := <-mpg123StdoutChan:
-			if strings.HasPrefix(line, "@F ") {
-				recentStatusLine = line
-			}
-			time.Sleep(1 * time.Millisecond)
-			continue
-		default:
-		}
-		break
+	statusLines := getLinesFromStdoutMatchingPrefix("@F ")
+	recentStatusLine := ""
+	if len(statusLines) > 0 {
+		recentStatusLine = statusLines[len(statusLines)-1]
 	}
+
+	// create JSON representation of frame info line
 	playbackInfo, err := getPlaybackInfoFromFrameLine(recentStatusLine)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -71,34 +104,12 @@ func getPlaybackInfo(w http.ResponseWriter, r *http.Request) {
 }
 
 func status(w http.ResponseWriter, r *http.Request) {
-	// drain the stdout channel to remove any unread stdout lines
-	for {
-		select {
-		case _ = <-mpg123StdoutChan:
-			// we sleep for a ms to allow concurrent goroutines to write
-			time.Sleep(1 * time.Millisecond)
-			continue
-		default:
-		}
-		break
-	}
-	io.WriteString(mpg123Stdin, "STATE\n")
+	drainStdout()
+	sendCommand("STATE")
 
-	// allow time for the application to read the input and respond
-	time.Sleep(30 * time.Millisecond)
-
-	// collect all available output as above
-	for {
-		select {
-		case line := <-mpg123StdoutChan:
-			if strings.HasPrefix(line, "@STATE") {
-				fmt.Fprintf(w, "%s\n", line)
-			}
-			time.Sleep(1 * time.Millisecond)
-			continue
-		default:
-		}
-		break
+	stateLines := getLinesFromStdoutMatchingPrefix("@STATE ")
+	for _, line := range stateLines {
+		fmt.Fprintf(w, fmt.Sprintf("%s\n", line))
 	}
 	fmt.Fprintf(w, "OK")
 }
